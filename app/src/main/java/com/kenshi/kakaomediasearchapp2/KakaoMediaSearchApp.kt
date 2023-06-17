38d09ee6d94b054e8c7593ad32b11c6d7283de2f
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-06-17 13:37:59 +0900

    fix: Application 클래스 내 코드 제거
    startup library 를 통해 이미 해주고 있음

--- app/src/main/java/com/kenshi/kakaomediasearchapp2/KakaoMediaSearchApp.kt
+++ app/src/main/java/com/kenshi/kakaomediasearchapp2/KakaoMediaSearchApp.kt
@@ -2,18 +2,6 @@ package com.kenshi.kakaomediasearchapp2
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
 
 @HiltAndroidApp
-class KakaoMediaSearchApp: Application() {
-    override fun onCreate() {
-        super.onCreate()
-        initTimber()
-    }
-
-    private fun initTimber() {
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-    }
-}
+class KakaoMediaSearchApp: Application()
