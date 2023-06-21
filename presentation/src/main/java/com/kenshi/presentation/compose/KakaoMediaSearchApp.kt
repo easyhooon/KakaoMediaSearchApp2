9a3ccd684de6bf27fde152b438367005fb10d253
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-06-21 22:26:46 +0900

    fix: theme block App Composable 외부로 이동

--- presentation/src/main/java/com/kenshi/presentation/compose/KakaoMediaSearchApp.kt
+++ presentation/src/main/java/com/kenshi/presentation/compose/KakaoMediaSearchApp.kt
@@ -3,6 +3,7 @@ package com.kenshi.presentation.compose
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -11,13 +12,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.kenshi.presentation.R
@@ -26,44 +26,45 @@ import com.kenshi.presentation.compose.navigation.Video
 import com.kenshi.presentation.compose.navigation.navigateSingleTopTo
 import com.kenshi.presentation.compose.navigation.searchTabRowScreens
 import com.kenshi.presentation.compose.ui.components.SearchTabRow
-import com.kenshi.presentation.compose.ui.theme.KakaoMediaSearchApp2Theme
 
 @Composable
 fun KakaoMediaSearchApp() {
-    KakaoMediaSearchApp2Theme {
-        val navController = rememberNavController()
-        var searchQuery by remember { mutableStateOf("") }
-        val currentBackStack by navController.currentBackStackEntryAsState()
-        val currentDestination = currentBackStack?.destination
-        val currentScreen =
-            searchTabRowScreens.find { it.route == currentDestination?.route } ?: Video
-        val context = LocalContext.current
+    val viewModel: SearchComposeViewModel = hiltViewModel()
+    val navController = rememberNavController()
+    val currentBackStack by navController.currentBackStackEntryAsState()
+    val currentDestination = currentBackStack?.destination
+    val currentScreen = searchTabRowScreens.find { it.route == currentDestination?.route } ?: Video
 
-        Surface(color = Color.White) {
-            Column {
-                OutlinedTextField(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp),
-                    value = searchQuery,
-                    onValueChange = { searchQuery = it },
-                    label = { Text(context.getString(R.string.search)) },
-                    leadingIcon = { Icon(Icons.Filled.Search, contentDescription = "Search Icon") }
-                )
-                SearchTabRow(
-                    allScreens = searchTabRowScreens,
-                    onTabSelected = { newScreen ->
-                        navController.navigateSingleTopTo(newScreen.route)
-                    },
-                    currentScreen = currentScreen
-                )
-                SearchNavHost(
-                    navController = navController,
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(top = 8.dp)
-                )
-            }
+    Surface(color = Color.White) {
+        Column {
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                value = viewModel.searchQuery,
+                singleLine = true,
+                onValueChange = {
+                    viewModel.updateSearchQuery(it)
+                },
+                label = { Text(stringResource(R.string.search)) },
+                leadingIcon = { Icon(Icons.Filled.Search, contentDescription = "Search Icon") },
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done
+                ),
+            )
+            SearchTabRow(
+                allScreens = searchTabRowScreens,
+                onTabSelected = { newScreen ->
+                    navController.navigateSingleTopTo(newScreen.route)
+                },
+                currentScreen = currentScreen
+            )
+            SearchNavHost(
+                navController = navController,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(top = 8.dp)
+            )
         }
     }
 }
--- presentation/src/main/java/com/kenshi/presentation/compose/SearchComposeActivity.kt
+++ presentation/src/main/java/com/kenshi/presentation/compose/SearchComposeActivity.kt
@@ -3,6 +3,7 @@ package com.kenshi.presentation.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.kenshi.presentation.compose.ui.theme.KakaoMediaSearchApp2Theme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,7 +11,9 @@ class SearchComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            KakaoMediaSearchApp()
+            KakaoMediaSearchApp2Theme {
+                KakaoMediaSearchApp()
+            }
         }
     }
 }
