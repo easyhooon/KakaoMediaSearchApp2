466031360b1fa0ff3cffd09612ab9d5d30594717
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-06-23 05:55:50 +0900

    design: 리스트 썸네일 이미지 radius 적용

--- presentation/src/main/java/com/kenshi/presentation/compose/ui/components/ImageCard.kt
+++ presentation/src/main/java/com/kenshi/presentation/compose/ui/components/ImageCard.kt
@@ -8,10 +8,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -39,7 +41,8 @@ fun ImageCard(
     ) {
         AsyncImage(
             modifier = Modifier
-                .size(180.dp, 120.dp),
+                .size(180.dp, 120.dp)
+                .clip(RoundedCornerShape(8.dp)),
             model = ImageRequest.Builder(context)
                 .data(imageItem.thumbnailUrl)
                 .build(),
--- presentation/src/main/java/com/kenshi/presentation/compose/ui/components/VideoCard.kt
+++ presentation/src/main/java/com/kenshi/presentation/compose/ui/components/VideoCard.kt
@@ -11,11 +11,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -45,12 +47,13 @@ fun VideoCard(
         Box {
             AsyncImage(
                 modifier = Modifier
-                    .size(180.dp, 120.dp),
+                    .size(180.dp, 120.dp)
+                    .clip(RoundedCornerShape(8.dp)),
                 model = ImageRequest.Builder(context)
                     .data(videoItem.thumbnail)
                     .build(),
                 contentScale = ContentScale.Crop,
-                contentDescription = "Video Thumbnail Image"
+                contentDescription = "Video Thumbnail Image",
             )
             Text(
                 modifier = Modifier
