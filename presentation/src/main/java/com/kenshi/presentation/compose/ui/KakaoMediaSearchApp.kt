ba5ae0af89532f1ac912d2892fc989ca29f4fd63
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-04-10 06:03:23 +0900

    Compose TextField 디자인 수정
    XML TextField 와 디자인이 같도록

--- presentation/src/main/java/com/kenshi/presentation/compose/ui/KakaoMediaSearchApp.kt
+++ presentation/src/main/java/com/kenshi/presentation/compose/ui/KakaoMediaSearchApp.kt
@@ -1,24 +1,37 @@
 package com.kenshi.presentation.compose.ui
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -49,31 +62,81 @@ fun KakaoMediaSearchApp(
 
     Surface(color = Color.White) {
         Column {
-            OutlinedTextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
+//            OutlinedTextField(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(8.dp),
+//                value = searchQuery,
+//                singleLine = true,
+//                onValueChange = { query ->
+//                    viewModel.updateSearchQuery(query)
+//                },
+//                leadingIcon = {
+//                    Icon(
+//                        Icons.Filled.Search,
+//                        contentDescription = "Search Icon"
+//                    )
+//                },
+//                keyboardOptions = KeyboardOptions(
+//                    imeAction = ImeAction.Done
+//                ),
+//                placeholder = {
+//                    Text(
+//                        text = stringResource(R.string.search),
+//                        fontWeight = FontWeight.Thin
+//                    )
+//                },
+//            )
+            BasicTextField(
                 value = searchQuery,
-                singleLine = true,
                 onValueChange = { query ->
                     viewModel.updateSearchQuery(query)
                 },
-                leadingIcon = {
-                    Icon(
-                        Icons.Filled.Search,
-                        contentDescription = "Search Icon"
-                    )
-                },
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Done
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .padding(horizontal = 8.dp),
+                textStyle = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    textAlign = TextAlign.Start
                 ),
-                placeholder = {
-                    Text(
-                        text = stringResource(R.string.search),
-                        fontWeight = FontWeight.Thin
-                    )
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done,
+                ),
+                decorationBox = { innerTextField ->
+                    Row(
+                        modifier = Modifier
+                            .background(color = Color.White, shape = RoundedCornerShape(8.dp))
+                            .border(
+                                border = BorderStroke(width = 1.dp, color = Color(0xFF111827)),
+                                shape = RoundedCornerShape(8.dp),
+                            ),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Spacer(modifier = Modifier.width(18.dp))
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "",
+                            tint = Color(0xFF111827),
+                        )
+                        Spacer(modifier = Modifier.width(width = 8.dp))
+                        Box(
+                        ) {
+                            if (searchQuery.isEmpty()) {
+                                Text(
+                                    text = stringResource(R.string.search),
+                                    color = Color(0xFF9ca3af),
+                                    fontWeight = FontWeight.Normal,
+                                )
+                            }
+                            innerTextField()
+                        }
+                    }
                 },
             )
+
             SearchTabRow(
                 allScreens = searchTabRowScreens,
                 onTabSelected = { newScreen ->
